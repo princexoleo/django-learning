a450dfc7974bfbb636c0eaaffde23bb02135d5bc
maz_leo <xtleon.xl@gmail.com>
2019-05-17 02:24:47 +0600

    templating with django

--- first_lesson/sampleapp/snippets/views.py
+++ first_lesson/sampleapp/snippets/views.py
@@ -5,7 +5,8 @@ from django.urls import reverse
 # Create your views here.
 def snippets_list(request):
     name =[ 'bob', 'jackson', 'leo', 'maz']
-    return render(request, 'snippets/snippets_list.html', {'name_key':name})
+    is_expired = False
+    return render(request, 'snippets/snippets_list.html', {'name_key':name, 'is_expired':is_expired})
 
 def snippets_details(request, id):
     return HttpResponse(f'snippets_favourite with id of {id}')
